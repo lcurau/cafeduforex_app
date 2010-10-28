4c1a2d0f1122a95f1b0eee5995c688396c1562e6
Laurent Curau <shoops@gmail.com>
2010-10-28 15:28:32 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CafeduforexApp::Application.config.secret_token = 'c032e1053ddb91ce7635c41a0461e0d3d01dd1c17210132036df947ea7a4e31fcd197f44c7b601c737dbf9e5b60d3148bcdfde22f93cc9e521a67b629b3a71cd'
